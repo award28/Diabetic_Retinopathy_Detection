7a09472229d52702abd8dd8386e790dc827fd16e
Atsuko <ashimiz1@binghamton.edu>
2018-01-28 08:48:50 -0500

    Added scores to images

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 from PIL import Image
 import pandas as pd
-from sys import argv
+import glob
+import csv
 
-im = Image.open("test_ds/train/" + argv[1] + "_left.jpeg") #Can be many different formats.
-pix = im.load()
-print(im.size) #Get the width and hight of the image for iterating over
-print(pix[0,0]) #Get the RGBA Value of the a pixel of an image
-pix[x,y] = value # Set the RGBA Value of the image (tuple)
-im.save("test.png") # Save the modified pixels as png
+data = pd.DataFrame()
+size = 0, 0
+scores = {} # filename : score (num 1-4)
+
+for filename in glob.glob('./test_ds/train/*.jpeg'):
+
+	im = Image.open(filename, 'r')
+	
+	width, height = im.size
+	
+	if (im.size > size):
+		size = width, height
+
+with open('test_ds/labels.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+	for row in readCSV:
+   		scores[row[0]] = row[1]
+   		#print("filename: " + row[0] + ", score: " + str(row[1]))
+
+for filename in glob.glob('./test_ds/train/*.jpeg'):
+	im = Image.open(filename, 'r')
+
+	new_width, new_height = size
+	
+	im = im.resize((new_width, new_height), Image.ANTIALIAS)
+		
+	px_vals = list(im.getdata())
+
+	data = pd.DataFrame([x[0]*256**2 + 256* x[1] + x[2] for x in px_vals]).transpose()
+	filename = filename.split('/')[3]
+
+	data["dr"] = scores[filename.split('.')[0]]
+	print("added {} for img {}".format(scores[filename.split('.')[0]], filename.split('.')[0] ))
+
